7a3b011f51bb5a00ffe75c747ec083641ac559b8
SandraNasasiira <79199579+SandraNasasiira@users.noreply.github.com>
2021-06-11 16:41:07 +0300

    Add files via upload

--- banka.py
+++ banka.py
@@ -11,11 +11,15 @@ class Account():
               self.transactions=[]
               self.withdrawal=[]
               self.save_loan=[]
-              
+
     def deposit(self,amount):
-        if amount<=0:
+         try:
+            10+ amount
+         except TypeError:
+             return f"The amount must be in figures"
+         if amount<=0:
              return f"hello {self.name},you have deposited {amount} to your account.Your new balance is {self.balance}"
-        else:
+         else:
              self.balance+=amount
              transactions={"amount":amount, "balance":self.balance ,"time":datetime.now(),"narration":"Deposit"}
              self.transactions.append(transactions)
@@ -26,22 +30,30 @@ class Account():
              return self.balance
 
     def withdraw(self,amount):
-        if amount<0:
+          try:
+            10+ amount
+          except TypeError:
+             return f"The amount must be in figures"
+          if amount<0:
              return f"you can't withdraw"
-        elif amount>self.balance:
+          elif amount>self.balance:
 
              return f"you have insufficient balance"
-        else:
+          else:
              self.balance-= amount
              return f"hello {self.name},you have withdrawn{amount} and your balance is{self.balance} "
     def borrow(self,amount):
-        if  amount>=self.loan_limit:
+          try:
+            10+ amount
+          except TypeError:
+             return f"The amount must be in figures"
+          if  amount>=self.loan_limit:
              return f"Dear {self.name}, the amount you are trying to borrow is above your loan limit, please deposit more to increase your loan limit"
-        elif self.loan>0:
+          elif self.loan>0:
              return f"Dear {self.name}, You still have not cleared your loan, please clear your outstanding debts so that you can borrow more thank you."
-        elif amount<=0:
+          elif amount<=0:
              return f"Dear {self.name}, You do not have enough to borrow"
-        else:
+          else:
              self.loan=1
              self.balance+=amount
              return f"Dear {self.name}, You have successfully borrowed {amount}. Your new balance is {self.balance}"
@@ -68,18 +80,67 @@ class Account():
            time     =save_loan["time"]
            print(f"{time.strftime('%D')}....{narration}....{amount}....Balance{balance}")
     def loan_payment(self,amount):
-          if amount<0:
-                return f"please put a positive amount"
-          elif amount<self.loan:
-              self.loan-=amount:
-                return f"Dear {self.name} you have paid {amount} on your loan"
-          else:
+         if amount<=0:
+              return f"please put a positive amount"
+         elif amount<self.loan:
+              self.loan-=amount
+              return f"Dear {self.name} you have paid {amount} on your loan"
+         else:
               difference=amount-self.loan
               self.balance+=difference
-                return f"dear {self.name}you have fully paid your loan and your balance is{self.balance}"
-    for loan_payment in self.save_loan:
-           narration=loan_payment["narration"]
-           amount   =sloan_payment["amount"]
-           loan_payment =loan_payment["loan_payment"]
-           time     =loan_payment["time"]
-           print(f"{time.strftime('%D')}....{narration}....{amount}....Balance{balance}")
+              loanpay={"amount": amount, "balance": self.balance, "Time":  now.strftime("%D"), "Narration": "loan payment"}
+              self.loanpays.append(loanpay)
+              return f"Dear {self.name} you have fully paid your loan, your account balance is {self.balance}" 
+    def get_statement(self):
+      for transaction in self.transactions:
+          narration = transaction["Narration"]
+          amount =transaction["amount"]
+          balance = transaction["balance"]
+          time = transaction["Time"]
+          print(f"Dear {self.name}, You made a {narration} of {amount} on {time}, your balance is {balance} thank you.")
+      for withdrawal in self.withdrawals:
+          narration = withdrawal["Narration"]
+          amount =withdrawal["amount"]
+          balance = withdrawal["balance"]
+          time = withdrawal["Time"]
+          print(f"Dear {self.name}, You made a {narration} of {amount} on {time}, your balance is {balance} thank you.")
+      for loan in self.loans:
+          narration = loan["Narration"]
+          amount =loan["amount"]
+          balance = loan["balance"]
+          time = loan["Time"]
+          print(f"Dear {self.name}, You acquired a {narration} of {amount} on {time}, your balance is {balance} thank you.")
+      for loanpay in self.loanpays:
+          narration = loanpay["Narration"]
+          amount =loanpay["amount"]
+          balance = loanpay["balance"]
+          time = loanpay["Time"]
+          print(f"Dear {self.name}, You made a {narration} of {amount} on {time}, your balance is {balance} thank you.")
+
+    def transfer(self,amount,account):
+         try:
+            10+ amount
+
+         except TypeError:
+               return f"The amount must be in figures"
+         if    amount+fee<self.balance:
+               return f"your balance is {self.balance},you need{amount+fee}"
+         else:
+               self.balance-=amount+fee
+               account.deposit(amount)
+               return f"you account balance is now {amount}"
+class MobileMoneyAccount(Account):
+     def __init__(self,name,phone,loan_limit,service_provider):
+            super().__init__(name,phone,loan_limit)
+            self.service_provider=service_provider
+            self.limit=300000
+
+     def buy_airtime(self,amount):
+            if amount<0:
+               return f"you can't buy airtime"
+            elif amount>self.balance:
+
+               return f"you have insufficient balance"
+            else:
+               self.balance-= amount
+               return f"hello {self.name},you have bought airtime for {self.phone} worth {amount} and your balance is{self.balance}"
